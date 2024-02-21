8bd5734290aef0da12dbf6375ef0d16f121e1a37
Alex <116348552+A1-lex@users.noreply.github.com>
2024-02-21 21:47:53 +0300

    Add files via upload

---
+++ tasks.js
@@ -0,0 +1,29 @@
+let btnSubmit= document.querySelector('button');
+let input = document.querySelector('input');
+
+input.addEventListener('keyup', () =>{
+   
+    btnSubmit.disabled = true;
+
+    if(input.value.length > 0){
+     
+        btnSubmit.disabled =false;
+    }
+});
+
+submit.addEventListener('click', () =>{
+    output.innerText = input.value;
+})
+
+function addLi(){
+    var task = document.getElementById('task').value,
+       listNode = document.getElementById('tasks'),
+       liNode = document.createElement("Li"),
+       txtNode = document.createTextNode(task);
+
+       liNode.appendChild(txtNode);
+
+       listNode.appendChild(liNode);
+
+
+}
